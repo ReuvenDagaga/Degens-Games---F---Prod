0feb69b266ca7651c8e77ad2f1e67a9852ccd7db
ReuvenDagaga <dagagareuven@gmail.com>
2025-03-27 19:45:42 +0200

    approute

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -8,6 +8,7 @@ import TokenomicsDashboard from "../pages/TokenomicsDashboardPage";
 import AIPlayerAnalytics from "../pages/AIPlayerAnalytics";
 import ChessPage from "../pages/Games/Chess/ChessPage";
 import Game2048Page from "../pages/Games/2048/2048Page";
+import TournamentDetailsPage from "../pages/TournamentDetailsPage";
 
 export const AppRoutes = () => {
 
@@ -19,7 +20,7 @@ export const AppRoutes = () => {
       <Route path="/staking" element={<StakingDashboard />} />
       <Route path="/leaderboard" element={<LeaderboardPage />} />
       <Route path="/gameDetails/:gameSlug/:entryFee" element={<GameDetailsPage />} />
-      <Route path="/TournamentDetails/:gameSlug/:entryFee" element={< />} />
+      <Route path="/TournamentDetails/:gameSlug/:entryFee" element={<TournamentDetailsPage />} />
       <Route path="/play/Chess" element={<ChessPage />}/>
       <Route path="/play/2048" element={<Game2048Page />}/>
       <Route path="/Statistics" element={<TokenomicsDashboard />} />
