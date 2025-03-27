2bfba2d39aaa3df735ebb0772e66107411689462
ReuvenDagaga <dagagareuven@gmail.com>
2025-03-27 18:52:35 +0200

    back ai

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -5,7 +5,7 @@ import StakingDashboard from "../pages/StakingDashboard";
 import LeaderboardPage from "../pages/LeaderboardPage";
 import GameDetailsPage from "../pages/GameDetailsPage";
 import TokenomicsDashboard from "../pages/TokenomicsDashboardPage";
-// import AIPlayerAnalytics from "../pages/AIPlayerAnalytics";
+import AIPlayerAnalytics from "../pages/AIPlayerAnalytics";
 import ChessPage from "../pages/Games/Chess/ChessPage";
 import Game2048Page from "../pages/Games/2048/2048Page";
 import StateIOClone from "../pages/Games/BitcoinConquestGame/BitcoinConquestPage";
@@ -23,7 +23,7 @@ export const AppRoutes = () => {
       <Route path="/play/Chess" element={<ChessPage />}/>
       <Route path="/play/2048" element={<Game2048Page />}/>
       <Route path="/Statistics" element={<TokenomicsDashboard />} />
-      <Route path="/AI" element={<StateIOClone />} />
+      <Route path="/AI" element={<AIPlayerAnalytics />} />
     </Routes>
   );
 };
